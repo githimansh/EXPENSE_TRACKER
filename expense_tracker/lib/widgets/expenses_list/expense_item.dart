8cdc9b71fe908616010a9a77f671ad551270ff5e
Himanshu kumar <himu2486@gmail.com>
2024-02-24 23:11:18 +0530

    first commit

---
+++ expense_tracker/lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,19 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget{
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+      child: Text(expense.title),
+    ),
+    );
+    
+  }
+}
