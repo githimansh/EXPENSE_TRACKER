4ede53d4e0632bc9f8c4ac5a2fed52bc11784b55
Himanshu kumar <himu2486@gmail.com>
2024-03-03 15:14:49 +0530

    add the them

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -2,10 +2,39 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor:const Color.fromARGB(208, 179, 132, 230),
+  );
 void main(){
   runApp(
     MaterialApp(
-    theme: ThemeData(useMaterial3: true),
+    theme: ThemeData().copyWith(
+          //  useMaterial3: true,
+           colorScheme: kColorScheme,
+           appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+           ),
+           cardTheme:const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16, 
+              vertical: 8
+              ),
+           ),
+           elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+           ),
+           textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 16,
+            ),
+          ),
+        ),
       home: const Expenses(),
     ),
   );
